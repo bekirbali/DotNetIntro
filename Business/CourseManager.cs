d550e521e8463a5f6a344f1550f6114d824ab55f
bekirbali <bfbali43@gmail.com>
2024-02-22 23:17:17 +0300

    first commit

---
+++ Business/CourseManager.cs
@@ -0,0 +1,38 @@
+﻿using kodlamaioLesson1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kodlamaioLesson1.Business;
+
+internal class CourseManager
+{
+    public CourseManager() {
+        Course course1 = new Course();
+        course1.Id = 1;
+        course1.Name = "c#";
+        course1.Description = ".Net8";
+        course1.Price = 0;
+
+        Course course2 = new Course();
+        course1.Id = 2;
+        course1.Name = "js";
+        course1.Description = "React";
+        course1.Price = 0;
+
+        Course course3 = new Course();
+        course1.Id = 3;
+        course1.Name = "python";
+        course1.Description = "Python 3.20";
+        course1.Price = 0;
+
+        Course[] courses = { course1, course2, course3 };
+    }
+
+    public void GetAll()
+    {
+        Console.WriteLine("courses listed");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+using kodlamaioLesson1.Business;
+using kodlamaioLesson1.Entities;
+
+Console.WriteLine("Hello, World!");
+
+
+string message1 = "krediler";
+int term = 12;
+double amount = 100000;
+bool isAuth = true;
+
+if (isAuth = false)
+{
+    
+    Console.WriteLine("could not logged in");
+}
+else
+{
+    Console.WriteLine("logged in succesfully");
+}
+
+string[] loans = ["loan1", "loan2", "loan3", "loan4", "loan5", "loan6"];
+
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+
+Course course1 = new Course();
+course1.Id = 1;
+course1.Name = "c#";
+course1.Description = ".Net8";
+course1.Price = 0;
+
+Course course2 = new Course();
+course1.Id = 2;
+course1.Name = "js";
+course1.Description = "React";
+course1.Price = 0;
+
+Course course3 = new Course();
+course1.Id = 3;
+course1.Name = "python";
+course1.Description = "Python 3.20";
+course1.Price = 0;
+
+Course[] courses = { course1, course2, course3 };
+
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Name);
+}
+
+
+CourseManager courseManager = new CourseManager();
+
+courseManager.GetAll();
+
+Console.WriteLine("code ended");
